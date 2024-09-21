a57ab40cac03b4293a339c29c3c0280205669bc6
the-dot-py <rahul81629@gmail.com>
2024-09-21 18:56:07 +0530

    Commited_1(rahul)
    Login +Signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'teal-100':'#E3FDFD',
+        'teal-200':'#CBF1F5',
+        'teal3-300':'#A6E3E9',
+        'teal4-400':'#71C9CE',
+      }
+    },
   },
   plugins: [],
 }
